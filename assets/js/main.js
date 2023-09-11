9e111a6f235f97834e1d4a170e7088ee2ad3e625
davidmhilton <dahilton@davidson.edu>
2023-09-11 16:46:38 -0400

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,8 @@
 
-/* javascript */
+console.log(
+    "Title: House Feeling \n",
+    "Author: THEOS \n",
+    "Release Date: April 28th, 2020",
+    "Link: https://open.spotify.com/track/37Mk3mXkPp45RXA6OSJnBE?si=899764ba0c88400c",
+    "License: 2020 Le Bon Son Records"
+);
