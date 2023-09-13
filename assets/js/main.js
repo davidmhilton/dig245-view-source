55794e98eacc3b6a7be44c7c50a091d89bfcca42
davidmhilton <dahilton@davidson.edu>
2023-09-13 14:50:51 -0400

    fix main.js path

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@
 console.log(
     "Title: House Feeling \n",
     "Author: THEOS \n",
-    "Release Date: April 28th, 2020",
+    "Release Date: April 28th, 2020\n",
+    "License: 2020 Le Bon Son Records\n",
     "Link: https://open.spotify.com/track/37Mk3mXkPp45RXA6OSJnBE?si=899764ba0c88400c",
-    "License: 2020 Le Bon Son Records"
-);
+)
